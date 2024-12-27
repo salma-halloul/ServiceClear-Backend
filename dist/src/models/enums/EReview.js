e7f2cb6e8e61f32f660d30f5e5e3cec1324be9df
Salma Halloul <salmahalloul52@gmail.com>
2024-12-27 01:20:15 +0100

    add build

--- dist/src/models/enums/EReview.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EReview = void 0;
-var EReview;
-(function (EReview) {
-    EReview["PENDING"] = "Pending";
-    EReview["APPROVED"] = "Approved";
-    EReview["REJECTED"] = "Rejected";
-})(EReview || (exports.EReview = EReview = {}));
